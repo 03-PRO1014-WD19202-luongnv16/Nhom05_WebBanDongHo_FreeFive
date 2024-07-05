57d038c4c472dd7db85c222f6c80dd699c4f86e3
haha <you@example.com>
2024-07-05 17:05:50 +0700

    PDO

--- index.php
+++ index.php
@@ -5,5 +5,12 @@ require_once "./commons/helper.php";
 require_once "./commons/connect-db.php";
 //controllers, models
 require_file(PATH_CONTROLLER);
+require_file(PATH_MODEL);
 
+//Điều hướng act
+$act = $_GET['act'] ?? '';
+
+match ($act) {
+    
+}
 require_once "./commons/disconnect-db.php";
