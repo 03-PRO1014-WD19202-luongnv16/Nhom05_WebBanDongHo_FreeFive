279dd51b492a41b4073cc0007f94648c454846ce
haha <you@example.com>
2024-07-06 16:20:19 +0700

    xóa env.php

--- index.php
+++ index.php
@@ -3,12 +3,13 @@
 require_once "./commons/env.php";
 require_once "./commons/helper.php";
 require_once "./commons/connect-db.php";
+require_once "./commons/model.php";
 //controllers, models
 require_file(PATH_CONTROLLER);
 require_file(PATH_MODEL);
 
 //Điều hướng act
-$act = $_GET['act'] ?? '';
+$act = $_GET['act'] ?? '/';
 
 match ($act) {
     
