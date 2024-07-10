8d30e0e2d797c1e65831fdf916752e8e704008cf
haha <you@example.com>
2024-07-10 21:11:39 +0700

    Thêm $scripts

--- admin/views/layouts/master.php
+++ admin/views/layouts/master.php
@@ -6,16 +6,25 @@
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>FREEFIVE ADMIN</title>
+    
+    <title><?= $title ?? 'Dashboard' ?> - Admin</title>
 
     <!-- Custom fonts for this template-->
     <link href="<?= BASE_URL ?>assets/admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="<?= BASE_URL ?>assets/admin/css/sb-admin-2.min.css" rel="stylesheet">
+
+    <?php 
+        if (isset($style) && $style) {
+            require_once PATH_VIEW_ADMIN . 'styles/' . $style . '.php';
+        }
+
+        if (isset($style2) && $style2) {
+            require_once PATH_VIEW_ADMIN . $style2 . '.php';
+        }
+    ?>
 
 </head>
 
@@ -39,7 +48,7 @@
                 <!-- End of Topbar -->
 
                 <!-- Begin Page Content -->
-                <?php require_once PATH_VIEW_ADMIN . $view . ".php"; ?>
+                <?php require_once PATH_VIEW_ADMIN . $view . '.php'; ?>
                 <!-- /.container-fluid -->
 
             </div>
@@ -73,11 +82,16 @@
     <!-- Custom scripts for all pages-->
     <script src="<?= BASE_URL ?>assets/admin/js/sb-admin-2.min.js"></script>
 
-    <?php
+    <?php 
         if (isset($script) && $script) {
-            require_once PATH_VIEW_ADMIN .'scripts/'. $script . ".php";
+            require_once PATH_VIEW_ADMIN . 'scripts/' . $script . '.php';
+        }
+
+        if (isset($script2) && $script2) {
+            require_once PATH_VIEW_ADMIN . $script2 . '.php';
         }
     ?>
+
 </body>
 
 </html>
