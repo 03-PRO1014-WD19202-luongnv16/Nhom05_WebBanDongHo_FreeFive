9fc8fff60c7fb29985174004ca6431e2dcf36ba5
haha <you@example.com>
2024-07-10 17:03:17 +0700

    fix lỗi login

--- admin/views/login/login.php
+++ admin/views/login/login.php
@@ -13,7 +13,9 @@
 
     <!-- Custom fonts for this template-->
     <link href="<?= BASE_URL ?>assets/admin/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
 
     <!-- Custom styles for this template-->
     <link href="<?= BASE_URL ?>assets/admin/css/sb-admin-2.min.css" rel="stylesheet">
@@ -41,31 +43,44 @@
                                     </div>
 
                                     <?php if (isset($_SESSION['error'])) : ?>
-                                        <div class="alert alert-danger">
-                                            <?= $_SESSION['error'] ?>
-                                        </div>
-                                        <?php unset($_SESSION['error']); ?>
+                                    <div class="alert alert-danger">
+                                        <?= $_SESSION['error'] ?>
+                                    </div>
+                                    <?php unset($_SESSION['error']); ?>
                                     <?php endif; ?>
 
                                     <form action="" method="POST" class="user">
                                         <div class="form-group">
-                                            <input type="email" class="form-control form-control-user" name="email" placeholder="Enter Email Address...">
+                                            <input type="email" class="form-control form-control-user" name="email"
+                                                placeholder="Enter Email Address...">
                                         </div>
                                         <div class="form-group">
-                                            <input type="password" class="form-control form-control-user" name="password" placeholder="Password">
+                                            <input type="password" class="form-control form-control-user"
+                                                name="password" placeholder="Password">
                                         </div>
-                                        <button type="submit" class="btn btn-primary btn-user btn-block">Login</a>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">Login</button>
+                                        <hr>
+                                        <a href="index.html" class="btn btn-google btn-user btn-block">
+                                            <i class="fab fa-google fa-fw"></i> Login with Google
+                                        </a>
+                                        <a href="index.html" class="btn btn-facebook btn-user btn-block">
+                                            <i class="fab fa-facebook-f fa-fw"></i> Login with Facebook
+                                        </a>
                                     </form>
+                                    <hr>
+                                    <div class="text-center">
+                                        <a class="small" href="forgot-password.html">Forgot Password?</a>
+                                    </div>
+                                    <div class="text-center">
+                                        <a class="small" href="register.html">Create an Account!</a>
+                                    </div>
                                 </div>
                             </div>
                         </div>
                     </div>
                 </div>
-
             </div>
-
         </div>
-
     </div>
 
     <!-- Bootstrap core JavaScript-->
